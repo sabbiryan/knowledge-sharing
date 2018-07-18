178471a7c23ae9c53a628b204804958fdbc268dd
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2018-07-18 22:26:22 +0600

    production script added

---
+++ Migration/Scripts/ProductionScript_20180718_9.30AM.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [QuestionAnswerViewCounts] (
+    [Id] int NOT NULL IDENTITY,
+    [CreationTime] datetime2 NOT NULL,
+    [CreatorUserId] bigint NULL,
+    [LastModificationTime] datetime2 NULL,
+    [LastModifierUserId] bigint NULL,
+    [DeleterUserId] bigint NULL,
+    [DeletionTime] datetime2 NULL,
+    [IsDeleted] bit NOT NULL,
+    [QuestionAnswerId] int NOT NULL,
+    [Count] bigint NOT NULL,
+    CONSTRAINT [PK_QuestionAnswerViewCounts] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_QuestionAnswerViewCounts_AbpUsers_CreatorUserId] FOREIGN KEY ([CreatorUserId]) REFERENCES [AbpUsers] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_QuestionAnswerViewCounts_QuestionAnswers_QuestionAnswerId] FOREIGN KEY ([QuestionAnswerId]) REFERENCES [QuestionAnswers] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_QuestionAnswerViewCounts_CreatorUserId] ON [QuestionAnswerViewCounts] ([CreatorUserId]);
+
+GO
+
+CREATE INDEX [IX_QuestionAnswerViewCounts_QuestionAnswerId] ON [QuestionAnswerViewCounts] ([QuestionAnswerId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20180710184545_QuestionAnswerViewCount_Model_Added', N'2.1.0-rtm-30799');
+
+GO
+
